7b9554ff513c68a188c6e3467f2cf1c45c040b2d
Justin Peng <jpeng@box.com>
2013-07-09 21:18:39 +0000

    Added final grader assignment.

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "no url specified";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,23 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertUrlExists = function(infile) {
+    var urlFile = "urlFile.html";
+    var outFile = "grader.json";
+    rest.get(infile).on('complete', function(result){
+	fs.writeFileSync(urlFile,result);
+	if(!fs.existsSync(urlFile)) {
+	    console.log('%s does not exist. Exiting...',urlFile);
+	    process.exit(1);
+	}
+
+	var checkJson = checkHtmlFile(urlFile, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+	fs.writeFileSync(outFile, outJson);
+    });
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,10 +84,19 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <page_url>', 'Path to specified url', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url == URL_DEFAULT){
+	console.log("Using local file.");
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+	// Would duplicate fs.writeFileSync here to output JSON to file.
+	} else {
+	console.log("Using specified url.");
+	}
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
